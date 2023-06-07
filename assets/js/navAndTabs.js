d7b8a20b27ef1d359ed1c7424003f422325602eb
iambinod <helloitsbinod@gmail.com>
2023-06-07 13:14:17 +0545

    feat: added dashboard menu

--- assets/js/navAndTabs.js
+++ assets/js/navAndTabs.js
@@ -1,5 +1,5 @@
 function openCity(evt, content) {
-  var i, tabcontent, tablinks;
+  let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
@@ -12,5 +12,4 @@ function openCity(evt, content) {
   evt.currentTarget.className += " active";
 }
 
-// Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
